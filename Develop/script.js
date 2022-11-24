7be65551f80ce7565ad59da1f5a4879f20bf0a0c
Max <max.belenchuk@gmail.com>
2022-11-24 19:47:51 -0400

    Local storage get item is now functional, when refreshed the textinput appears on the schedule. Added more time slots as well.

--- Develop/script.js
+++ Develop/script.js
@@ -2,23 +2,24 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
+
+
+
 var today = dayjs();
 $('#specificdate').text(today.format('dddd, MMM YYYY'));
+
 
 $('.saveBtn').on('click', function () {
   var sibling = $(this).prev().val();
   console.log(sibling)
   var parent = $(this).parent().attr('id');
-  
+  console.log(parent)
   localStorage.setItem(parent, sibling);
-  
 })
 
 function timeNow() {
   var blockTime = $('.hour')
-  console.log(blockTime);
   var currentTime = dayjs();
-  console.log(currentTime.$H);
 
   for (let index = 0; index < blockTime.length; index++) {
     if (blockTime[index].dataset.hour < currentTime.$H) {
@@ -26,21 +27,30 @@ function timeNow() {
       $(blockTime[index]).parent().removeClass('present');
       $(blockTime[index]).parent().addClass('past');
     }
-  
+
     else if (blockTime[index].dataset.hour == currentTime.$H) {
       $(blockTime[index]).parent().removeClass('future');
       $(blockTime[index]).parent().removeClass('past');
       $(blockTime[index]).parent().addClass('present');
     }
-  
+
     else {
       $(blockTime[index]).parent().removeClass('past');
       $(blockTime[index]).parent().removeClass('present');
       $(blockTime[index]).parent().addClass('future');
     }
-    
+
   }
-  
+  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+
 }
 timeNow();
 
@@ -54,7 +64,14 @@ timeNow();
 
 
 
-$(function () {
+
+
+
+
+
+
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -73,4 +90,4 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+
